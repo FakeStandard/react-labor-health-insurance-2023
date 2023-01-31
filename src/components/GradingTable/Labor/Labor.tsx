421bf9794000dfe46393d9ebcfdf2f94237e00f2
riley.lin <75247434+FakeStandard@users.noreply.github.com>
2023-01-31 14:59:57 +0800

    Update Labor.tsx

--- src/components/GradingTable/Labor/Labor.tsx
+++ src/components/GradingTable/Labor/Labor.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './Labor.css'
+import './Labor.css';
 import { Card, Col, Container, Row, Stack, Table } from "react-bootstrap";
 import { ILaborInfo } from "../../../models/ILaborInfo";
 
